091b99f91cf58da73434a42a2d6f1a9dee2ed91f
Hiroshi Urabe <mail@torounit.com>
2024-12-07 14:50:55 +0900

    add post

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  console.log(JSON.stringify(body));
+  return c.text("OK");
+});
+
 export default app;
