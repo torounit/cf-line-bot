3d17b2f0b8873fb97ac6b7c9d78020846c7d131b
Hiroshi Urabe <mail@torounit.com>
2025-01-18 05:47:17 +0900

    toLocaleString

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,9 @@ app.post("/", async (c) => {
       messages: [
         {
           role: "system",
-          content: `You are a friendly assistant. You answer in Japanese. The current unix time is ${new Date().getTime()}. Timezone is Asia/Tokyo.`,
+          content: `
+          You are a friendly assistant. You answer in Japanese.
+          Today is ${new Date().toLocaleString('en', { timeZone: 'Asia/Tokyo' })}. Timezone is Asia/Tokyo`,
         },
         ...messages,
       ],
